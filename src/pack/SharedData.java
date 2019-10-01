56291f1bb65e07f4885c62e86fdcabf932f93655
shyong0427 <shyong0427@daum.net>
2019-10-01 17:22:34 +0900

    2019-10-1 Spring_Web_자원공유 Update Ver.1

---
+++ src/pack/SharedData.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class SharedData {
+	private String shared;
+
+	public String getShared() {
+		return shared;
+	}
+
+	public void setShared(String shared) {
+		this.shared = shared;
+	}
+}
